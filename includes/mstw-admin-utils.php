3c4c7193109822b8e18932b991f914e08b6c4def
MarkODonnell <MarkODonnell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 03:49:17 +0000

    Added if( !function_exists ) around all _utl_ functions.
    git-svn-id: https://plugins.svn.wordpress.org/game-locations/trunk@905247 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/mstw-admin-utils.php
+++ includes/mstw-admin-utils.php
@@ -26,15 +26,17 @@
  *		This function is used by the require_once statement to figure
  *		out whether or not to load the utils.
  *---------------------------------------------------------------*/
-	function mstw_admin_utils_loaded( ) {
+if( !function_exists( 'mstw_admin_utils_loaded' ) ) {
+ function mstw_admin_utils_loaded( ) {
 		return( true );
 	}
-
+}
 
  /*----------------------------------------------------------------	
  *	MSTW_UTL_BUILD_CSS_RULE
  *		Simple convenience function to build css rules from $options.
- *---------------------------------------------------------------*/	
+ *---------------------------------------------------------------*/
+if( !function_exists( 'mstw_utl_build_css_rule' ) ) { 
 	function mstw_utl_build_css_rule( $options_array, $option_name, $css_rule ) {
 		if ( isset( $options_array[$option_name] ) and !empty( $options_array[$option_name] ) ) {
 			return $css_rule . ":" . $options_array[$option_name] . "; \n";	
@@ -43,6 +45,7 @@
 			return "";
 		}
 	}
+}
  /*----------------------------------------------------------------	
  *	MSTW_UTL_COLOR_CTRL
  *	Builds color selector controls for the admin UI
@@ -56,6 +59,7 @@
  *
  *	return - none. Control is displayed.
  *---------------------------------------------------------------*/
+if( !function_exists( 'mstw_utl_color_ctrl' ) ) {	
 	function mstw_utl_color_ctrl( $args ) { 
 		$id = $args['id'];
 		$name = $args['name'];
@@ -68,7 +72,7 @@
 		echo ( $label != '' ? "<br /><span class='description'>$label</span>" : "" );
 
 	} //End: mstw_utl_color_ctrl
- 
+ }
  /*----------------------------------------------------------------	
  *	MSTW_UTL_TEXT_CTRL
  *	Builds text format controls for the admin UI
@@ -81,6 +85,7 @@
  *
  *	return - none. Control is displayed.
  *---------------------------------------------------------------*/
+if( !function_exists( 'mstw_utl_text_ctrl' ) ) {	
 	function mstw_utl_text_ctrl( $args ) { 
 		$id = $args['id'];
 		$name = $args['name'];
@@ -92,7 +97,7 @@
 		echo ( $label != '' ? "<br /><span class='description'>$label</span>" : "" );
 		
 	} //End: mstw_utl_text_ctrl
-	
+}	
 /*----------------------------------------------------------------	
  *	MSTW_UTL_CHECKBOX_CTRL
  *	Builds checkbox format controls for the admin UI
@@ -107,6 +112,7 @@
  *
  *	Return - none. Control is displayed.
  *---------------------------------------------------------------*/
+if( !function_exists( 'mstw_utl_checkbox_ctrl' ) ) {	
 	function mstw_utl_checkbox_ctrl( $args ) { 
 		$id = 		$args['id'];
 		$name = 	$args['name'];
@@ -119,7 +125,7 @@
 		echo ( $label != '' ? "<br /><span class='description'>$label</span>" : "" );
 		
 	}	//End: mstw_utl_checkbox_ctrl
-	
+}	
 /*----------------------------------------------------------------	
  *	Shortcut to build 'Show-Hide' Select-Option controls for the admin UI
  *	Just like mstw_select_option_ctrl with hard-wired options - 
@@ -133,6 +139,7 @@
  *
  *	Return - none. Output is echoed.
  *---------------------------------------------------------------*/	
+if( !function_exists( 'mstw_utl_show_hide_ctrl' ) ) {	
 	function mstw_utl_show_hide_ctrl( $args ) {
 	
 		$new_args = array(	'options' => array(	__( 'Show', 'mstw-loc-domain' ) => 1, 
@@ -147,7 +154,7 @@
 		mstw_utl_select_option_ctrl( $new_args );
 		
 	}  //End: mstw_utl_show_hide_ctrl
-	
+}
 /*----------------------------------------------------------------	
  *	Builds Select-Option controls for the admin UI
  *
@@ -160,6 +167,7 @@
  *
  *	Return - none. Output is echoed.
  *---------------------------------------------------------------*/
+if( !function_exists( 'mstw_utl_select_option_ctrl' ) ) {	
 	function mstw_utl_select_option_ctrl( $args ) {
 		
 		$options = $args['options'];
@@ -180,7 +188,7 @@
 		//echo "<label for='$id'>$label</label> \n";
 		
 	}  //End: mstw_utl_select_option_ctrl
-
+}
 //----------------------------------------------------------------	
 //	Builds date format controls for the admin UI
 //
@@ -194,6 +202,7 @@
 //
 //	return - none. Output is echoed.
 //---------------------------------------------------------------
+if( !function_exists( 'mstw_utl_date_format_ctrl' ) ) {	
 	function mstw_utl_date_format_ctrl( $args ) {
 	
 		//extract( $args );
@@ -276,7 +285,7 @@
 		*/
 		
 	}
-	
+}
 /*----------------------------------------------------------------	
  *	Builds time format controls for the admin UI
  *
@@ -287,6 +296,7 @@
  *
  *	return - none. Output is echoed.
  *---------------------------------------------------------------*/
+if( !function_exists( 'mstw_utl_time_format_ctrl' ) ) {		
 	function mstw_utl_time_format_ctrl( $args ) {
 		// need the $mstw_time_formats array
 		$mstw_utl_time_formats = array ( 
@@ -322,11 +332,11 @@
 		echo "</select>" . __( 'Formats for', 'mstw-loc-domain' ) . " 08:00";
 		
 	}
-	
-	
+}	
 /*----------------------------------------------------------------	
  *	Sanitization Functions
  *---------------------------------------------------------------*/	
+if( !function_exists( 'mstw_utl_sanitize_hex_color' ) ) {	
 	function mstw_utl_sanitize_hex_color( $color ) {
 		// Check $color for proper hex color format (3 or 6 digits) or the empty string.
 		// Returns corrected string if valid hex color, returns null otherwise
@@ -340,176 +350,179 @@
 
 		return null;
 	}
-
+}
+if( !function_exists( 'mstw_utl_sanitize_number' ) ) {
 	function mstw_utl_sanitize_number( $number ) {
 
 	}
-	
+}	
 //----------------------------------------------------------------	
 // Helper function for registering admin form field settings
 // This is new and will aggregate all the mstw_utl__ctrl() functions (someday!)
 //----------------------------------------------------------------
-function mstw_utl_build_form_field( $args ) {
-	// default array to overwrite when calling the function
-	/*
-	$defaults = array(
-		'id'      => 'default_field', // the ID of the setting in our options array, and the ID of the HTML form element
-		'title'   => 'Default Field',  // the label for the HTML form element
-		'desc'    => 'This is a default description.', // the description displayed under the HTML form element
-		'std'     => '',  // the default value for this setting
-		'type'    => 'text', // the HTML form element to use
-		'section' => 'main_section', // settings section to which this setting belongs
-		'choices' => array(), // (optional): the values in radio buttons or a drop-down menu
-		'class'   => ''  // the HTML form element class. Also used for validation purposes!
-	);
-	*/
-	// "extract" to be able to use the array keys as variables in our function output below
-	//extract( wp_parse_args( $args, $defaults ) );
-	extract( $args );
-	
-	// additional arguments for use in form field output in the function wptuts_form_field_fn!
-	/*
-	$field_args = array(
-		'type'      => $type,
-		'id'        => $id,
-		'desc'      => $desc,
-		'value'     => $value,
-		'choices'   => $choices,
-		'label_for' => $id,
-		'class'     => $class,
-		'name'		=> $name,
-	);
-	
-	echo '<p>Build form field</p>';
-	return;
+if( !function_exists( 'mstw_utl_build_form_field' ) ) {
+	function mstw_utl_build_form_field( $args ) {
+		// default array to overwrite when calling the function
+		/*
+		$defaults = array(
+			'id'      => 'default_field', // the ID of the setting in our options array, and the ID of the HTML form element
+			'title'   => 'Default Field',  // the label for the HTML form element
+			'desc'    => 'This is a default description.', // the description displayed under the HTML form element
+			'std'     => '',  // the default value for this setting
+			'type'    => 'text', // the HTML form element to use
+			'section' => 'main_section', // settings section to which this setting belongs
+			'choices' => array(), // (optional): the values in radio buttons or a drop-down menu
+			'class'   => ''  // the HTML form element class. Also used for validation purposes!
+		);
+		*/
+		// "extract" to be able to use the array keys as variables in our function output below
+		//extract( wp_parse_args( $args, $defaults ) );
+		extract( $args );
+		
+		// additional arguments for use in form field output in the function wptuts_form_field_fn!
+		/*
+		$field_args = array(
+			'type'      => $type,
+			'id'        => $id,
+			'desc'      => $desc,
+			'value'     => $value,
+			'choices'   => $choices,
+			'label_for' => $id,
+			'class'     => $class,
+			'name'		=> $name,
+		);
+		
+		echo '<p>Build form field</p>';
+		return;
 
-	/*
-	add_settings_field( $id, 
-						$title, 
-						'mstw_utl_display_form_field', 
-						$display_on_page, 
-						$page_section, 
-						$field_args 
-						);
-	*/
+		/*
+		add_settings_field( $id, 
+							$title, 
+							'mstw_utl_display_form_field', 
+							$display_on_page, 
+							$page_section, 
+							$field_args 
+							);
+		*/
+	}
 }
-
 //----------------------------------------------------------------	
 // Helper function for building HTML for all admin form fields
 // All fields will share this function (someday!)
 // Called by mstw_utl_build_ctrl() and echoes output
 //----------------------------------------------------------------
-
-function mstw_utl_display_form_field( $args ) {
-	
-	extract( $args );
-	/*
-	// get the settings sections array
-	/*
-	$settings_output 	= wptuts_get_settings();
-	
-	$wptuts_option_name = $settings_output['wptuts_option_name'];
-	$options 			= get_option($wptuts_option_name);
-	
-	// pass the standard value if the option is not yet set in the database
-	if ( !isset( $options[$id] ) && 'type' != 'checkbox' ) {
-		$options[$id] = $std;
-	}
-	
-	// Additional field class. Output only if the class is defined in the $args()
-	$field_class = ($class != '') ? ' ' . $class : '';
-	
-	
-	// switch html display based on the setting type.
-	switch ( $type ) {
-		/*
-		case 'text':
-			$options[$id] = stripslashes($options[$id]);
-			$options[$id] = esc_attr( $options[$id]);
-			echo "<input class='regular-text$field_class' type='text' id='$id' name='" . $wptuts_option_name . "[$id]' value='$options[$id]' />";
-			echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
-		break;
+if( !function_exists( 'mstw_utl_display_form_field' ) ) {
+	function mstw_utl_display_form_field( $args ) {
 		
-		case "multi-text":
-			foreach($choices as $item) {
-				$item = explode("|",$item); // cat_name|cat_slug
-				$item[0] = esc_html__($item[0], 'wptuts_textdomain');
-				if (!empty($options[$id])) {
-					foreach ($options[$id] as $option_key => $option_val){
-						if ($item[1] == $option_key) {
-							$value = $option_val;
+		extract( $args );
+		/*
+		// get the settings sections array
+		/*
+		$settings_output 	= wptuts_get_settings();
+		
+		$wptuts_option_name = $settings_output['wptuts_option_name'];
+		$options 			= get_option($wptuts_option_name);
+		
+		// pass the standard value if the option is not yet set in the database
+		if ( !isset( $options[$id] ) && 'type' != 'checkbox' ) {
+			$options[$id] = $std;
+		}
+		
+		// Additional field class. Output only if the class is defined in the $args()
+		$field_class = ($class != '') ? ' ' . $class : '';
+		
+		
+		// switch html display based on the setting type.
+		switch ( $type ) {
+			/*
+			case 'text':
+				$options[$id] = stripslashes($options[$id]);
+				$options[$id] = esc_attr( $options[$id]);
+				echo "<input class='regular-text$field_class' type='text' id='$id' name='" . $wptuts_option_name . "[$id]' value='$options[$id]' />";
+				echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+			break;
+			
+			case "multi-text":
+				foreach($choices as $item) {
+					$item = explode("|",$item); // cat_name|cat_slug
+					$item[0] = esc_html__($item[0], 'wptuts_textdomain');
+					if (!empty($options[$id])) {
+						foreach ($options[$id] as $option_key => $option_val){
+							if ($item[1] == $option_key) {
+								$value = $option_val;
+							}
+						}
+					} else {
+						$value = '';
+					}
+					echo "<span>$item[0]:</span> <input class='$field_class' type='text' id='$id|$item[1]' name='" . $wptuts_option_name . "[$id|$item[1]]' value='$value' /><br/>";
+				}
+				echo ($desc != '') ? "<span class='description'>$desc</span>" : "";
+			break;
+			
+			case 'textarea':
+				$options[$id] = stripslashes($options[$id]);
+				$options[$id] = esc_html( $options[$id]);
+				echo "<textarea class='textarea$field_class' type='text' id='$id' name='" . $wptuts_option_name . "[$id]' rows='5' cols='30'>$options[$id]</textarea>";
+				echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : ""; 		
+			break;
+			
+			case 'select':
+				echo "<select id='$id' class='select$field_class' name='" . $wptuts_option_name . "[$id]'>";
+					foreach($choices as $item) {
+						$value 	= esc_attr($item, 'wptuts_textdomain');
+						$item 	= esc_html($item, 'wptuts_textdomain');
+						
+						$selected = ($options[$id]==$value) ? 'selected="selected"' : '';
+						echo "<option value='$value' $selected>$item</option>";
+					}
+				echo "</select>";
+				echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : ""; 
+			break;
+			
+			case 'select2':
+				echo "<select id='$id' class='select$field_class' name='" . $wptuts_option_name . "[$id]'>";
+				foreach($choices as $item) {
+					
+					$item = explode("|",$item);
+					$item[0] = esc_html($item[0], 'wptuts_textdomain');
+					
+					$selected = ($options[$id]==$item[1]) ? 'selected="selected"' : '';
+					echo "<option value='$item[1]' $selected>$item[0]</option>";
+				}
+				echo "</select>";
+				echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+			break;
+			
+			
+			// This is live
+			case 'checkbox':
+				echo "<input class='checkbox$field_class' type='checkbox' id='$id' name='$name' value='$value' " . checked( $value, 1, false ) . " />";
+				echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+			break;
+			
+			/*
+			case "multi-checkbox":
+				foreach($choices as $item) {
+					
+					$item = explode("|",$item);
+					$item[0] = esc_html($item[0], 'wptuts_textdomain');
+					
+					$checked = '';
+					
+					if ( isset($options[$id][$item[1]]) ) {
+						if ( $options[$id][$item[1]] == 'true') {
+							$checked = 'checked="checked"';
 						}
 					}
-				} else {
-					$value = '';
-				}
-				echo "<span>$item[0]:</span> <input class='$field_class' type='text' id='$id|$item[1]' name='" . $wptuts_option_name . "[$id|$item[1]]' value='$value' /><br/>";
-			}
-			echo ($desc != '') ? "<span class='description'>$desc</span>" : "";
-		break;
-		
-		case 'textarea':
-			$options[$id] = stripslashes($options[$id]);
-			$options[$id] = esc_html( $options[$id]);
-			echo "<textarea class='textarea$field_class' type='text' id='$id' name='" . $wptuts_option_name . "[$id]' rows='5' cols='30'>$options[$id]</textarea>";
-			echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : ""; 		
-		break;
-		
-		case 'select':
-			echo "<select id='$id' class='select$field_class' name='" . $wptuts_option_name . "[$id]'>";
-				foreach($choices as $item) {
-					$value 	= esc_attr($item, 'wptuts_textdomain');
-					$item 	= esc_html($item, 'wptuts_textdomain');
 					
-					$selected = ($options[$id]==$value) ? 'selected="selected"' : '';
-					echo "<option value='$value' $selected>$item</option>";
+					echo "<input class='checkbox$field_class' type='checkbox' id='$id|$item[1]' name='" . $wptuts_option_name . "[$id|$item[1]]' value='1' $checked /> $item[0] <br/>";
 				}
-			echo "</select>";
-			echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : ""; 
-		break;
-		
-		case 'select2':
-			echo "<select id='$id' class='select$field_class' name='" . $wptuts_option_name . "[$id]'>";
-			foreach($choices as $item) {
-				
-				$item = explode("|",$item);
-				$item[0] = esc_html($item[0], 'wptuts_textdomain');
-				
-				$selected = ($options[$id]==$item[1]) ? 'selected="selected"' : '';
-				echo "<option value='$item[1]' $selected>$item[0]</option>";
-			}
-			echo "</select>";
-			echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
-		break;
-		
-		
-		// This is live
-		case 'checkbox':
-			echo "<input class='checkbox$field_class' type='checkbox' id='$id' name='$name' value='$value' " . checked( $value, 1, false ) . " />";
-			echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
-		break;
-		
-		/*
-		case "multi-checkbox":
-			foreach($choices as $item) {
-				
-				$item = explode("|",$item);
-				$item[0] = esc_html($item[0], 'wptuts_textdomain');
-				
-				$checked = '';
-				
-			    if ( isset($options[$id][$item[1]]) ) {
-					if ( $options[$id][$item[1]] == 'true') {
-			   			$checked = 'checked="checked"';
-					}
-				}
-				
-				echo "<input class='checkbox$field_class' type='checkbox' id='$id|$item[1]' name='" . $wptuts_option_name . "[$id|$item[1]]' value='1' $checked /> $item[0] <br/>";
-			}
-			echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
-		break;
-		
+				echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";
+			break;
+			
+		}
+		*/
 	}
-	*/
 }
 ?>
